cd5b9cde9da200d8f625560ffebe13af6c507fef
Mariana Theml <marianatheml@gmail.com>
2022-05-08 14:28:19 -0300

    feat: Add migrations to create table students on mysql

---
+++ src/migrations/20220508170944_create_table_students.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('students', function (table) {
+            table.increments('id').primary();
+            table.string('name', 255).notNullable();
+            table.string('cpf', 14).notNullable().unique();
+            table.string('email', 255).notNullable().unique();
+
+            table.timestamp('created_at').defaultTo(knex.fn.now())
+            table.timestamp('updated_at').defaultTo(knex.fn.now())
+        })
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTable('students');
+};
+
+exports.config = { transaction: false };
